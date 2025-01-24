7585528ff0658b6286b8dbfae6b0cca33c45e6e7
ColdTbrew <shchoi8687@hotmail.com>
2025-01-24 15:32:32 +0900

    solved problem w1

---
+++ Seung-hyuk/Week_1_problem_1.sql
@@ -0,0 +1,4 @@
+SELECT animal_ins.animal_id AS ANIMAL_ID, animal_ins.name AS NAME
+FROM animal_ins JOIN animal_outs ON animal_ins.animal_id = animal_outs.animal_id
+WHERE animal_ins.DATETIME > animal_outs.DATETIME
+ORDER BY animal_ins.DATETIME
