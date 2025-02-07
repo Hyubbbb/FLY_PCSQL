05efed227c5688b2f00e97df5d9a58b5d983ab07
G1 <xxurfall@gmail.com>
2025-02-07 19:02:18 +0900

    정렬을 까먹지 말자.sql

--- Ji-won/Week_3_problem_1.sql
+++ Ji-won/Week_3_problem_1.sql
@@ -1,3 +1,9 @@
--- 프로그래머스, 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기
--- https://school.programmers.co.kr/learn/courses/30/lessons/151139
+-- 프로그래머스, 부서별 평균 연봉 조회하기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/284529
 
+SELECT E.DEPT_ID, D.DEPT_NAME_EN, ROUND(AVG(E.SAL)) AS AVG_SAL
+FROM HR_EMPLOYEES E
+LEFT JOIN HR_DEPARTMENT D
+ON E.DEPT_ID = D.DEPT_ID
+GROUP BY E.DEPT_ID
+ORDER BY AVG_SAL DESC;
