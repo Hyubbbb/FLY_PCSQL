15a2bbdf6fdb6d2e64fcd03c7d89554f0de6529c
G1 <xxurfall@gmail.com>
2025-02-07 18:31:47 +0900

    효울적인 범주화.sql

--- Ji-won/Week_3_problem_2.sql
+++ Ji-won/Week_3_problem_2.sql
@@ -1,2 +1,7 @@
 -- 프로그래머스, 가격대 별 상품 개수 구하기
 -- https://school.programmers.co.kr/learn/courses/30/lessons/131530
+
+SELECT FLOOR(PRICE/10000)*10000 AS PRICE_GROUP, COUNT(*) AS PRODUCTS
+FROM PRODUCT
+GROUP BY PRICE_GROUP
+ORDER BY PRICE_GROUP;
