7116ce70402a39f0fd59ad5f76e21913c8984654
G1 <xxurfall@gmail.com>
2025-02-11 09:24:26 +0900

    정렬!!!!!

--- Ji-won/Week_4_problem_1.sql
+++ Ji-won/Week_4_problem_1.sql
@@ -1,1 +1,9 @@
--- 프로그래머스, 
+-- 프로그래머스, 상품 별 오프라인 매출 구하기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131533
+
+SELECT P.PRODUCT_CODE, P.PRICE*SUM(S.SALES_AMOUNT) AS SALES
+FROM PRODUCT P
+JOIN OFFLINE_SALE S 
+ON P.PRODUCT_ID = S.PRODUCT_ID
+GROUP BY P.PRODUCT_ID
+ORDER BY SALES DESC, PRODUCT_CODE;
