1cc771f0f6f4bb6f3c738674112dc9cf2306bf94
G1 <xxurfall@gmail.com>
2025-02-21 22:05:23 +0900

    JOIN 연습하기

--- Ji-won/Week_5_problem_1.sql
+++ Ji-won/Week_5_problem_1.sql
@@ -1,6 +1,7 @@
--- 프로그래머스, 중복 제거하기
--- https://school.programmers.co.kr/learn/courses/30/lessons/59408
+-- 프로그래머스, 대장균들의 자식의 수 구하기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/299305
 
-SELECT COUNT(DISTINCT NAME) count
-FROM ANIMAL_INS
-WHERE NAME is NOT NULL;
+SELECT A.ID, COUNT(B.ID) AS CHILD_COUNT
+FROM ECOLI_DATA A
+LEFT JOIN ECOLI_DATA B ON A.ID = B.PARENT_ID
+GROUP BY A.ID
